31d3536666ae04d71ea1dab593d2db220c72e444
du64 <ducheng0@protonmail.com>
2023-02-02 22:59:15 -0600

    Update main.rs

--- payout-calculator/src/main.rs
+++ payout-calculator/src/main.rs
@@ -2,28 +2,22 @@ use std::io;
 use std::{f32};
 
 fn main() {
-    println!(">> Du's kinda meh payout calculator <<\n");
+    println!(">> Addons payout calculator <<\n");
     loop {
-        logic()
+        println!("How many downloads? ");
+        let mut var1 = String::new();
+        io::stdin().read_line(&mut var1).expect("Parsing error");
+
+        println!("What's the eCPM? ");
+        let mut var2 = String::new();
+        io::stdin().read_line(&mut var2).expect("Parsing error");
+
+        let dl: f32 = var1.trim().parse().ok().expect("Numbers only");
+        let cpm: f32 = var2.trim().parse().ok().expect("Numbers only");
+        let count: f32 = 1000.0; // Divide the download count by this, 1K default
+        let split: f32 = 0.7; // Payments split, 70/30 default
+        let answer: i32 = (dl / count * cpm * split) as i32; // Does the calculation
+        println!("The monthly payment comes out to: >> ${answer} <<\n");
+        println!("-==-==-==-==-")
     }
-}
-
-fn logic() {
-    println!("How many downloads? ");
-    let mut var1 = String::new();
-    io::stdin().read_line(&mut var1).expect("Parsing error");
-
-    println!("What's the eCPM? ");
-    let mut var2 = String::new();
-    io::stdin().read_line(&mut var2).expect("Parsing error");
-
-    let dl: f32 = var1.trim().parse().ok().expect("Numbers only");
-    let cpm: f32 = var2.trim().parse().ok().expect("Numbers only");
-    let count: f32 = 1000.0; // Divide the download count by this, 1K default
-    let split: f32 = 0.7; // Payments split, 70/30 default
-    let answer: f32 = dl / count * cpm * split; // Does the calculation
-
-    let output = answer as i32; // Converts floating point to integer
-
-    println!("The monthly payment comes out to: >> ${output} <<");
 }
